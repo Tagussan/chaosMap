6a9c682df5b6015a5cadca91ff982c116beaf021
Tagussan <jap.yoshi@gmail.com>
2015-08-06 16:13:26 +0900

    sample set added

--- logisticMap.v
+++ logisticMap.v
@@ -1,46 +1,48 @@
 module logisticModule(CLK, RST, red, green, blue, row, col, mu, maxrepeat);
    input CLK, RST;
-   input [8:0] maxrepeat;
+   input [9:0] maxrepeat;
    output red, green, blue;
    input [9:0] row, col;
+   wire [9:0] row_inverse;
    input [17:0] mu;
-   wire [16:0] A_result, B_result, C_result, D_result, E_result, F_result, G_result;
-   wire [9:0] A_row, B_row, C_row, D_row, E_row, F_row, G_row;
+   wire [16:0] A_result, B_result, C_result, D_result, E_result, F_result;//, G_result;
+   wire [9:0] A_row, B_row, C_row, D_row, E_row, F_row;//, G_row;
    wire [2:0] colorbits;
+   assign row_inverse = 580 - row;
    logisticCycle A_cycle(.CLK(CLK), .RST(RST), .dzero(17'b0_1000_0010_0100_0000), .times(maxrepeat), .mu(mu), .result(A_result));
-   logisticCycle B_cycle(.CLK(CLK), .RST(RST), .dzero(17'b0_1000_0010_0100_0001), .times(maxrepeat), .mu(mu), .result(B_result));
-   logisticCycle C_cycle(.CLK(CLK), .RST(RST), .dzero(17'b0_1000_0010_0100_0010), .times(maxrepeat), .mu(mu), .result(C_result));
-   logisticCycle D_cycle(.CLK(CLK), .RST(RST), .dzero(17'b0_1000_0010_0100_0011), .times(maxrepeat), .mu(mu), .result(D_result));
-   logisticCycle E_cycle(.CLK(CLK), .RST(RST), .dzero(17'b0_1000_0010_0100_0100), .times(maxrepeat), .mu(mu), .result(E_result));
-   logisticCycle F_cycle(.CLK(CLK), .RST(RST), .dzero(17'b0_1000_0010_0100_0101), .times(maxrepeat), .mu(mu), .result(F_result));
-   logisticCycle G_cycle(.CLK(CLK), .RST(RST), .dzero(17'b0_1000_0010_0100_0110), .times(maxrepeat), .mu(mu), .result(G_result));
+   logisticCycle B_cycle(.CLK(CLK), .RST(RST), .dzero(17'b0_1000_0010_0100_0010), .times(maxrepeat), .mu(mu), .result(B_result));
+   logisticCycle C_cycle(.CLK(CLK), .RST(RST), .dzero(17'b0_1000_0010_0100_0110), .times(maxrepeat), .mu(mu), .result(C_result));
+   logisticCycle D_cycle(.CLK(CLK), .RST(RST), .dzero(17'b0_1000_0010_0100_1000), .times(maxrepeat), .mu(mu), .result(D_result));
+   logisticCycle E_cycle(.CLK(CLK), .RST(RST), .dzero(17'b0_1000_0010_0100_1010), .times(maxrepeat), .mu(mu), .result(E_result));
+   logisticCycle F_cycle(.CLK(CLK), .RST(RST), .dzero(17'b0_1000_0010_0100_1101), .times(maxrepeat), .mu(mu), .result(F_result));
+   //logisticCycle G_cycle(.CLK(CLK), .RST(RST), .dzero(17'b0_1000_0010_0100_0110), .times(maxrepeat), .mu(mu), .result(G_result));
    assign A_row = A_result >> 8;
    assign B_row = B_result >> 8;
    assign C_row = C_result >> 8;
    assign D_row = D_result >> 8;
    assign E_row = E_result >> 8;
    assign F_row = F_result >> 8;
-   assign G_row = G_result >> 8;
+   //assign G_row = G_result >> 8;
    function [2:0] colorselect;
       input [9:0] row,col;
-      if(0 < col && col <= 20 && A_row <= row && row <= A_row + 5)
+      if(0 < col && col <= 110 && A_row <= row && row <= A_row + 5)
          colorselect = 3'b001;
-      else if (20 < col && col <= 40 && B_row <= row && row <= B_row + 5)
+      else if (110 < col && col <= 220 && B_row <= row && row <= B_row + 5)
          colorselect = 3'b011;
-      else if (40 < col && col <= 60 && C_row <= row && row <= C_row + 5)
+      else if (220 < col && col <= 330 && C_row <= row && row <= C_row + 5)
          colorselect = 3'b100;
-      else if (60 < col && col <= 80 && D_row <= row && row <= D_row + 5)
+      else if (330 < col && col <= 440 && D_row <= row && row <= D_row + 5)
          colorselect = 3'b010;
-      else if (80 < col && col <= 100 && E_row <= row && row <= E_row + 5)
+      else if (440 < col && col <= 550 && E_row <= row && row <= E_row + 5)
          colorselect = 3'b101;
-      else if (100 < col && col <= 120 && F_row <= row && row <= F_row + 5)
+      else if (550 < col && col <= 660 && F_row <= row && row <= F_row + 5)
             colorselect = 3'b110;
-      else if (120 < col && col <= 140 && G_row <= row && row <= G_row + 5)
-         colorselect = 3'b000;
+      //else if (660 < col && col <= 770 && G_row <= row && row <= G_row + 5)
+      //   colorselect = 3'b000;
       else
          colorselect = 3'b111;
    endfunction
-   assign colorbits = colorselect(row, col);
+   assign colorbits = colorselect(row_inverse, col);
    assign {red,green,blue} = colorbits;
 endmodule
 
@@ -52,12 +54,12 @@ module logisticCycle(CLK, RST, done, dzero, times, mu, result);
 //repeating and stop sanitary
    input CLK, RST;
    input [16:0] dzero;
-   input [8:0] times;
+   input [9:0] times;
    input [17:0] mu;
    wire [16:0] funcIn, funcOut;
    output [16:0] result;
    output done;
-   reg [8:0] ind; //ind as how many times mapped
+   reg [12:0] ind; //ind as how many times mapped
    reg done;
    reg [16:0] result;
    always @(posedge CLK) begin
--- sample_set.v
+++ sample_set.v
@@ -2,27 +2,36 @@ module sample_set(CLK, RST, sample_num, mu, maxrepeat, calc_clock);
    input [5:0] sample_num;
    input CLK, RST;
    output [17:0] mu;
-   output [8:0] maxrepeat;
+   output [9:0] maxrepeat;
    output calc_clock;
    wire CLK_SLOW, CLK_VERY_SLOW;
    divider_very_slow divider_very_slow(.clk(CLK), .rst(RST), .clkout(CLK_VERY_SLOW));
    divider_slow divider_slow(.clk(CLK), .rst(RST), .clkout(CLK_SLOW));
+   //sample 0: a = 0.99, SLOW
    function [17:0] select_mu;
       input [5:0] sample_num;
       case (sample_num)
-         0:     select_mu = 18'b10_1101_1011_1101_1111;
-         1:     select_mu = 18'b11_1101_1011_1101_1111;
-         2:     select_mu = 18'b11_1101_1011_1100_1111;
+         0:     select_mu = 18'b00_1100_1100_1100_1100;
+         1:     select_mu = 18'b01_0011_0011_0011_0011;
+         2:     select_mu = 18'b11_0001_1001_1001_1001;
+         3:     select_mu = 18'b11_0111_0011_0011_0011;
+         4:     select_mu = 18'b11_1000_1100_1100_1101;
+         5:     select_mu = 18'b11_1001_0000_1010_0100;
+         6:     select_mu = 18'b11_1001_0000_1110_0101;
+         7:     select_mu = 18'b11_1001_0001_0010_0111;
+         8:     select_mu = 18'b11_1001_0001_1110_1100;
+         9:     select_mu = 18'b11_1001_0100_0111_1011;
+         10:    select_mu = 18'b11_1111_1111_1111_1111;
          default: select_mu = 0;
       endcase
    endfunction
-   function [8:0] select_maxrepeat;
+   function [12:0] select_maxrepeat;
       input [5:0] sample_num;
       case (sample_num)
-         0:     select_maxrepeat = 500;
-         1:     select_maxrepeat = 256;
-         2:     select_maxrepeat = 256;
-         default: select_maxrepeat = 0;
+         0:     select_maxrepeat = 512;
+         1:     select_maxrepeat = 512;
+         2:     select_maxrepeat = 512;
+         default: select_maxrepeat = 512;
       endcase
    endfunction
    function select_CLK;
@@ -30,7 +39,7 @@ module sample_set(CLK, RST, sample_num, mu, maxrepeat, calc_clock);
       case (sample_num)
          0:     select_CLK = CLK_SLOW;
          1:     select_CLK = CLK_SLOW;
-         2:     select_CLK = CLK_VERY_SLOW;
+         2:     select_CLK = CLK_SLOW;
          default:  select_CLK = CLK_SLOW;
       endcase
    endfunction
